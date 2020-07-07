e255c51eb60edee6786ca8a68b29966f53dc0222
Gogs <gogs@fake.local>
2020-07-07 22:15:48 +0200

    Init exos Ruby (semaine 2)

---
+++ pyramide_mario.rb
@@ -0,0 +1,17 @@
+# INIT NUMBER
+number = 0
+
+# TANT QUE NOMBRE N'EST PAS ENTRE 1 ET 25 DEMANDE À L'UTILISATEUR
+begin 
+   puts "Entrez un nombre entre 1 et 25 : "
+   print ">"
+   number = gets.to_i
+end while number < 1 or number > 25
+
+i = 1
+while 0 < number
+   print " " * (number-1) + "#" * i + "\n"
+
+   number -= 1
+   i += 1
+end
